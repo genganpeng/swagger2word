d8b196e79a76351a859a52f97248c6c742b34cf0
puhaiyang <761396462@qq.com>
2022-08-21 23:36:11 +0800

    tmp

--- src/main/java/org/word/model/ApiTplExcelData.java
+++ src/main/java/org/word/model/ApiTplExcelData.java
@@ -1,12 +1,11 @@
 package org.word.model;
 
-import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Data;
 
 @Data
 public class ApiTplExcelData {
     private Integer index;
-    private String apiDocUrl;
+    private String swaggerUrl;
     private String apiPathUrl;
     private String apiMethod;
     private String title;
--- src/main/java/org/word/service/impl/ExportServiceImpl.java
+++ src/main/java/org/word/service/impl/ExportServiceImpl.java
@@ -6,14 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.word.model.ApiTplExcelData;
 import org.word.model.Table;
-import org.word.parser.SwaggerParserContext;
 import org.word.service.ExportService;
 import org.word.service.WordService;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -28,26 +28,36 @@ public class ExportServiceImpl implements ExportService {
     @Override
     public Map<String, Object> renderTableList(List<ApiTplExcelData> apiTplExcelDataList) {
         List<Table> tableReusltList = new ArrayList<>();
-        apiTplExcelDataList.stream().parallel().forEach(apiItem -> {
+        //根据swagger的接口url进行分组
+        Map<String, List<ApiTplExcelData>> swaggerUrlListMap = apiTplExcelDataList.stream().parallel().collect(Collectors.groupingBy(ApiTplExcelData::getSwaggerUrl));
+        Iterator<Map.Entry<String, List<ApiTplExcelData>>> swaggerUrlIt = swaggerUrlListMap.entrySet().iterator();
+        while (swaggerUrlIt.hasNext()) {
+            Map.Entry<String, List<ApiTplExcelData>> entry = swaggerUrlIt.next();
+            String swaggerUrl = entry.getKey();
+            List<ApiTplExcelData> apiDataList = entry.getValue();
             //获取出解析后的tableList
-            List<Table> tableList = tableService.getTableList(apiItem.getApiDocUrl());
-            //在里面找到对应的url
-            tableList.forEach(tableItem -> {
-                if (apiItem.getApiPathUrl().equals(tableItem.getUrl())) {
-                    if (StringUtils.isNotBlank(apiItem.getApiMethod())) {
+            List<Table> tableList = tableService.getTableList(swaggerUrl);
+            //将list转为map,提高查询时间复杂度
+            Map<String, List<Table>> urlTableListMap = tableList.stream().parallel().collect(Collectors.groupingBy(Table::getUrl));
+
+            apiDataList.stream().parallel().forEach(apiDataItem -> {
+                List<Table> subTables = urlTableListMap.get(apiDataItem.getApiPathUrl());
+                for (Table subTableItem : subTables) {
+                    if (StringUtils.isNotBlank(apiDataItem.getApiMethod())) {
                         //excel中传了请求method时，判断请求类型是否匹配
-                        if (apiItem.getApiMethod().equalsIgnoreCase(tableItem.getRequestType())) {
+                        if (apiDataItem.getApiMethod().equalsIgnoreCase(subTableItem.getRequestType())) {
                             //请求类型和url都匹配
-                            tableReusltList.add(tableItem);
+                            tableReusltList.add(subTableItem);
                         } else {
-                            log.info("[renderTableList] method不匹配.url:{} method1:{} method2:{}", tableItem.getUrl(), apiItem.getApiMethod(), tableItem.getRequestType());
+                            log.info("[renderTableList] method不匹配.url:{} method1:{} method2:{}", apiDataItem.getApiPathUrl(), apiDataItem.getApiMethod(), subTableItem.getRequestType());
                         }
                     } else {
-                        tableReusltList.add(tableItem);
+                        tableReusltList.add(subTableItem);
                     }
                 }
             });
-        });
+        }
+        //TODO 返回数据
         return null;
     }
 }
